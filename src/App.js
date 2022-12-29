d4adfb0d1fe9548089180d19f3673308deb1daae
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-29 19:26:37 +0100

    Thiming

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
 import "./App.css";
 import "./styles.css";
 
-import { AnimationLogo, SubmitButton } from "./components/Button/Button.styles";
+import {
+  AnimationLogo,
+  DarkButton,
+  SubmitButton,
+} from "./components/Button/Button.styles";
 import StyledButton, { FancyButton } from "./components/Button/Button";
 
+import { ThemeProvider } from "styled-components";
 import logo from "./logo.svg";
+
+const theme = {
+  dark: {
+    primary: "#000",
+    text: "#fff",
+  },
+  light: {
+    primary: "#fff",
+    text: "#000",
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
-      <AnimationLogo src={logo} />
+        <AnimationLogo src={logo} />
 
-      <StyledButton>Styled Button</StyledButton>
-      <div>
-        <br />
+        <StyledButton>Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+
+        <StyledButton variant="outlined">Styled Button</StyledButton>
+
+        <div>
+          <br />
+        </div>
+
+        <FancyButton as="a">Fancy Button</FancyButton>
+
+        <div>
+          <br />
+        </div>
+
+        <SubmitButton>Submit</SubmitButton>
+
+        <div>
+          <br />
+        </div>
+
+        <DarkButton>Dark Button</DarkButton>
       </div>
-
-      <StyledButton variant="outlined">Styled Button</StyledButton>
-
-      <div>
-        <br />
-      </div>
-
-      <FancyButton as="a">Fancy Button</FancyButton>
-
-      <div>
-        <br />
-      </div>
-
-      <SubmitButton>Submit</SubmitButton>
-    </div>
+    </ThemeProvider>
   );
 }
 
