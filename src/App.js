88b1040838706484e62348b2e73ec3ff04ee45a0
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-29 19:18:19 +0100

    Animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import "./styles.css";
 
+import { AnimationLogo, SubmitButton } from "./components/Button/Button.styles";
 import StyledButton, { FancyButton } from "./components/Button/Button";
 
-import { SubmitButton } from "./components/Button/Button.styles";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+      <AnimationLogo src={logo} />
+
       <StyledButton>Styled Button</StyledButton>
       <div>
         <br />
