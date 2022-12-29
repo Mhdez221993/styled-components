7b5299492c242b1738ae54340788db03d3d2e8ca
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-29 19:33:56 +0100

    Global styles

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import {
   SubmitButton,
 } from "./components/Button/Button.styles";
 import StyledButton, { FancyButton } from "./components/Button/Button";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
-import { ThemeProvider } from "styled-components";
 import logo from "./logo.svg";
 
 const theme = {
@@ -20,11 +20,19 @@ const theme = {
     primary: "#fff",
     text: "#000",
   },
+  fontFamily: "Segoe UI",
 };
+
+const GlobalStyle = createGlobalStyle`
+  button {
+    font-family: ${(props) => props.theme.fontFamily};
+  }
+`;
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <div className="App">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
